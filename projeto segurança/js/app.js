aca963ca0899837f199594f440b96909269a2bde
ronilson-neto28 <ronilsonneto2001@gmail.com>
2025-05-26 12:43:50 -0300

    alteraçoes no front

--- projeto segurança/js/app.js
+++ projeto segurança/js/app.js
@@ -9,3 +9,22 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+IMask(document.querySelector('input[name="cpf"]'), {
+    mask: '000.000.000-00'
+});
+
+IMask(document.querySelector('input[name="numero"]'), {
+    mask: '(00) 00000-0000'
+});
+
+// Mostrar/ocultar senha
+document.querySelectorAll('.toggle-password').forEach(icon => {
+    icon.addEventListener('click', () => {
+        const input = document.querySelector(icon.getAttribute('toggle'));
+        const isPassword = input.getAttribute('type') === 'password';
+        input.setAttribute('type', isPassword ? 'text' : 'password');
+        icon.classList.toggle('fa-eye');
+        icon.classList.toggle('fa-eye-slash');
+    });
+});
